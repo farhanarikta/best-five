12a134b8d79b6d1f1298f0f5b13bb031fa2b0bbf
farhanarikta <farhnarikta@gmail.com>
2022-08-23 23:48:05 +0600

    final commit

---
+++ main.js
@@ -0,0 +1,124 @@
+//Declaring All Variables
+const playerNames = document.querySelectorAll(".player-name");
+const playerInput = document.getElementById("player");
+const managerInput = document.getElementById("manager");
+const coachInput = document.getElementById("coach");
+const calculateBtn = document.getElementById("calculate");
+const calculateTotalBtn = document.getElementById("calculate-total");
+const playerExpense = document.getElementById("player-expense");
+const totalExpense = document.getElementById("total-expense");
+const playerContainer = document.getElementById("players");
+const addCustomBtn = document.getElementById("custom-add");
+
+const selectedPlayerList = document.getElementById("selected-players");
+const imageLink = document.querySelector(".image-link");
+const playerName = document.querySelector(".player-name-add");
+
+let selectedPlayerCount = 0;
+
+//Calculating All Players Expense
+calculateBtn.addEventListener("click", (event) => {
+  if (selectedPlayerCount === 0) {
+    alert("Select at least one player...");
+  } else if (playerInput.value == "") {
+    alert("Enter Player Expense...");
+  } else if (playerInput.value < 0) {
+    alert("Enter positive amount...");
+  } else {
+    const perPlayerExpense = +playerInput.value;
+    let expense =
+      selectedPlayerCount > 5
+        ? 5 * perPlayerExpense
+        : selectedPlayerCount * perPlayerExpense;
+    playerExpense.innerText = `$${expense}`;
+  }
+});
+
+//Calculating Total expense
+calculateTotalBtn.addEventListener("click", (event) => {
+  if (selectedPlayerCount === 0) {
+    alert("Select at least one player...");
+  } else if (playerInput.value == "") {
+    alert("Enter player expense...");
+  } else if (managerInput.value == "" || coachInput.value == "") {
+    alert("Enter manager's & coach's salary...");
+  } else if (managerInput.value < 0 || coachInput.value < 0) {
+    alert("Enter positive amount...");
+  } else {
+    let perPlayerExpense = +playerInput.value;
+    let playerExpense =
+      selectedPlayerCount > 5
+        ? 5 * perPlayerExpense
+        : selectedPlayerCount * perPlayerExpense;
+    let coachExpense = +coachInput.value;
+    let managerExpense = +managerInput.value;
+
+    let totalExpenses = playerExpense + managerExpense + coachExpense;
+    totalExpense.innerText = `$${totalExpenses}`;
+  }
+});
+
+//Getting The Player Name Which Button Will Be Clicked
+function getSelectedPlayerName(event) {
+  return event.target.parentElement.firstElementChild.innerText;
+}
+
+//Creating List Item And Inserting Player Name To That List Item InnerText
+function getSelectedPlayer(event) {
+  const playerName = getSelectedPlayerName(event);
+  let player = document.createElement("li");
+  player.classList.add("text-orange-300", "font-semibold");
+  player.innerText = playerName;
+  return player;
+}
+
+//Dynamically Adding Player Card
+addCustomBtn.addEventListener("click", (e) => {
+  if (imageLink.value === "" || playerName.value === "") {
+    alert("Please provide image link and card title...");
+  } else {
+    const divElement = document.createElement("div");
+    divElement.innerHTML = `
+    <div class="player rounded">
+    <img src="${imageLink.value}" alt="" class="rounded-t" />
+    <div
+      id="text"
+      class="text-white bg-gray-900 p-2 text-center rounded-b"
+    >
+      <h2
+        class="text-lg sm:text-2xl font-semibold tracking-wide player-name text-orange-300"
+      >
+          ${playerName.value}
+      </h2>
+      <p class="text-base font-medium mb-4">
+        36 Goals &nbsp; &middot; &nbsp; 120 Assists
+      </p>
+      <button class="w-full mb-2 select-btn btn-primary p-1 onclick='addPlayer()'">
+        Select
+      </button>
+    </div>
+  </div>
+    `;
+    selectBtns = document.querySelectorAll(".select-btn");
+    playerContainer.appendChild(divElement);
+    imageLink.value = "";
+    playerName.value = "";
+    alert("Card Added");
+  }
+});
+
+//Appending List Item To The List And Disabling The Button
+playerContainer.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    selectedPlayerCount++;
+    if (selectedPlayerCount > 5) {
+      alert(`You Can't Add More Than 5 Players...`);
+    } else {
+      const selectedPlayer = getSelectedPlayer(event);
+      selectedPlayerList.appendChild(selectedPlayer);
+      event.target.style.backgroundColor = "#60a5fa";
+      event.target.disabled = true;
+    }
+  }
+  event.stopPropagation();
+});
